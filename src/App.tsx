720e5292e65603a31b398c005b38b329e686f1d2
Yuval Komash <yuval@moveoboost.com>
2022-03-21 16:37:26 +0200

    first upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useEffect } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AllPokemons from './pages/AllPokemons';
+import PokemonProfile from './pages/PokemonProfile';
+import pokemon_logo from "./images/pokemon_logo.png";
+import { getPokemons } from "./Helper/services";
+import styled from 'styled-components';
 
+const HeaderImageStyle = styled.div`
+display: flex;
+justify-content: center;
+
+& img{
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+@media (max-width: 4150px) {
+
+  width: fit-content;
+  margin: auto;
+  & img{
+    width: 80%
+  }
+}
+`;
 function App() {
+
+  useEffect(() => {
+    getPokemons("0", "898").then(res => localStorage.setItem("pokemons", JSON.stringify(res?.pokemonsData))).catch(err => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <HeaderImageStyle>
+        <img src={pokemon_logo} alt="pokemon logo   " />
+      </HeaderImageStyle>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/pokedex" />} />
+          <Route path="/pokedex" element={<AllPokemons />} />
+          <Route path="/pokedex/:pokemonName" element={<PokemonProfile />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+
   );
 }
 
